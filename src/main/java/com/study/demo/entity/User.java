39aa92ce1a3140621af54c5358a109bc22b57e1f
Bliss49 <kyo8747@naver.com>
2025-04-21 17:49:10 +0900

    feat: user enitiy 생성

---
+++ src/main/java/com/study/demo/entity/User.java
@@ -0,0 +1,29 @@
+package com.study.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    private String id;
+
+    private String writer;
+
+    private String editor;
+
+    private String email;
+
+    private boolean status;
+
+    private String note;
+
+    private LocalDateTime createdAt; // 생성된 시간
+    private LocalDateTime updatedAt; // 업데이트 시간
+}
