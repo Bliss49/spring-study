ec570160cca232eb2ad213a74f0c3107c92bf544
Bliss49 <kyo8747@naver.com>
2025-04-22 14:55:12 +0900

    feat: 생성자 주입

--- src/main/java/com/study/demo/controller/UserController.java
+++ src/main/java/com/study/demo/controller/UserController.java
@@ -2,11 +2,13 @@ package com.study.demo.controller;
 
 import com.study.demo.entity.User;
 import com.study.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
     private UserService userService;
 
     @PostMapping
